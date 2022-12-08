baead49aca3efa02452fbb3db0a3ac7570c6aa4f
Princekumar2022 <pk279183@gmail.com>
2022-12-08 13:48:30 +0530

    bugs free

---
+++ src/routes/route.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const { authenticate, authorization } = require("../middleware/auth")
+const { createUser, loginUser, updateUser } = require('../controllers/userController')
+
+
+
+
+
+//..............................Test API.........................//
+
+router.get("/test-me" , function(req,res){
+    res.status(200).send({status:true,message:"Testing API"})
+})
+
+
+
+//........................................User API............................................//
+
+// create
+router.post('/register', createUser)
+
+// user login
+router.post('/login', loginUser)
+
+// update user
+router.put('/user/:userId', authenticate, authorization, updateUser)
+
+
+
+
+
+
+
+
+
+router.all("/*", (req, res) => { res.status(400).send({ status: false, message: "Endpoint is not correct plese provide a proper end-point" }) })
+
+
+
+
+module.exports = router
